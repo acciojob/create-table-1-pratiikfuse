0f6efed1147ca4fc1390b0185f3bbb24bf351f37
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-10 20:44:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table = document.getElementById("sampleTable");
+	let tr = document.createElement("tr");
+
+	let td1  = document.createElement("td");
+	let td2  = document.createElement("td");
+
+	td1.innerText = "new Cell1";
+	td2.innerText = "new Cell2";
+
+	tr.append(td1,td2);
+	let tr1  = document.querySelectorAll("#sampleTable>tr")[0];
+	table.insertBefore(tr,tr1);
+	
 }
